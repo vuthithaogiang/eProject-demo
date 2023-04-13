e384e7bdb9b1466334f76e9f65243ce4a67f72a9
giang <vuthithaogiang050400@gmail.com>
2023-04-13 10:13:08 +0700

    Add section lastest About Us

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -5,6 +5,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon, divIcon, point } from 'leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import images from '~/assets/images';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -66,72 +67,78 @@ function AboutUs() {
         });
     };
     return (
-        <div className={cx('container')}>
-            <div className={cx('keep-commerce-human')}>
-                <img src={images.human} />
-                <div>
-                    <h2>Keep Commerce Human</h2>
-                    <p>
-                        Mirrors is the global marketplace for unique and creative goods. It’s home to a universe of
-                        special, extraordinary items, from unique handcrafted pieces to vintage treasures.
-                    </p>
-                    <p>
-                        In a time of increasing automation, it’s our mission to keep human connection at the heart of
-                        commerce. That’s why we built a place where creativity lives and thrives because it’s powered by
-                        people. We help our community of sellers turn their ideas into successful businesses. Our
-                        platform connects them with millions of buyers looking for an alternative - something special
-                        with a human touch, for those moments in life that deserve imagination.
-                    </p>
-                    <p>
-                        As a company, we strive to lead with our guiding principles and to help spread ideas of
-                        sustainability and responsibility whose impact can reach far beyond our own business.
-                    </p>
+        <div className={cx('wrapper')}>
+            <div className={cx('container')}>
+                <div className={cx('keep-commerce-human')}>
+                    <img src={images.human} />
+                    <div>
+                        <h2>Keep Commerce Human</h2>
+                        <p>
+                            Mirrors is the global marketplace for unique and creative goods. It’s home to a universe of
+                            special, extraordinary items, from unique handcrafted pieces to vintage treasures.
+                        </p>
+                        <p>
+                            In a time of increasing automation, it’s our mission to keep human connection at the heart
+                            of commerce. That’s why we built a place where creativity lives and thrives because it’s
+                            powered by people. We help our community of sellers turn their ideas into successful
+                            businesses. Our platform connects them with millions of buyers looking for an alternative -
+                            something special with a human touch, for those moments in life that deserve imagination.
+                        </p>
+                        <p>
+                            As a company, we strive to lead with our guiding principles and to help spread ideas of
+                            sustainability and responsibility whose impact can reach far beyond our own business.
+                        </p>
+                    </div>
                 </div>
             </div>
 
             <div className={cx('team')}>
-                <div className={cx('staff')}>
-                    <p>Mirrors Staff</p>
-                    <h2>Meet Our Leadership</h2>
+                <div className={cx('container')}>
+                    <div className={cx('staff')}>
+                        <p>Mirrors Staff</p>
+                        <h2>Meet Our Leadership</h2>
 
-                    <div className={cx('staff-list')}>
-                        {/* item 1 */}
-                        <article className={cx('staff-item')}>
-                            <figure>
-                                <img className={cx('avatar')} src={images.staff1} alt="" />
-                            </figure>
-                            <p className={cx('staff-title')}>Chief Marketing Officer</p>
-                            <h3 className={cx('staff-name')}>Meghan Samit</h3>
-                        </article>
+                        <div className={cx('staff-list')}>
+                            {/* item 1 */}
+                            <article className={cx('staff-item')}>
+                                <figure>
+                                    <img className={cx('avatar')} src={images.staff1} alt="" />
+                                </figure>
+                                <p className={cx('staff-title')}>Chief Marketing Officer</p>
+                                <h3 className={cx('staff-name')}>Meghan Samit</h3>
+                            </article>
 
-                        {/* item 2 */}
-                        <article className={cx('staff-item')}>
-                            <figure>
-                                <img className={cx('avatar')} src={images.staff2} alt="" />
-                            </figure>
-                            <p className={cx('staff-title')}>Chief Human Resources Officer</p>
-                            <h3 className={cx('staff-name')}>Alissa Silva</h3>
-                        </article>
-                        {/* item 3 */}
-                        <article className={cx('staff-item')}>
-                            <figure>
-                                <img className={cx('avatar')} src={images.staff3} alt="" />
-                            </figure>
-                            <p className={cx('staff-title')}>Chief Technology Officer</p>
-                            <h3 className={cx('staff-name')}>Cindy Harris</h3>
-                        </article>
-                        {/* item 4 */}
-                        <article className={cx('staff-item')}>
-                            <figure>
-                                <img className={cx('avatar')} src={images.staff4} alt="" />
-                            </figure>
-                            <p className={cx('staff-title')}>Chief Product Officer</p>
-                            <h3 className={cx('staff-name')}>Lucas Tony</h3>
-                        </article>
+                            {/* item 2 */}
+                            <article className={cx('staff-item')}>
+                                <figure>
+                                    <img className={cx('avatar')} src={images.staff2} alt="" />
+                                </figure>
+                                <p className={cx('staff-title')}>Chief Human Resources Officer</p>
+                                <h3 className={cx('staff-name')}>Alissa Silva</h3>
+                            </article>
+                            {/* item 3 */}
+                            <article className={cx('staff-item')}>
+                                <figure>
+                                    <img className={cx('avatar')} src={images.staff3} alt="" />
+                                </figure>
+                                <p className={cx('staff-title')}>Chief Technology Officer</p>
+                                <h3 className={cx('staff-name')}>Cindy Harris</h3>
+                            </article>
+                            {/* item 4 */}
+                            <article className={cx('staff-item')}>
+                                <figure>
+                                    <img className={cx('avatar')} src={images.staff4} alt="" />
+                                </figure>
+                                <p className={cx('staff-title')}>Chief Product Officer</p>
+                                <h3 className={cx('staff-name')}>Lucas Tony</h3>
+                            </article>
+                        </div>
+
+                        <h5>The people who work at Mirrors share the vision and values of our community.</h5>
+                        <h5>
+                            We’re driven by the idea that the best work is born from diligence, craftsmanship and fun.
+                        </h5>
                     </div>
-
-                    <h5>The people who work at Mirrors share the vision and values of our community.</h5>
-                    <h5>We’re driven by the idea that the best work is born from diligence, craftsmanship and fun.</h5>
                 </div>
             </div>
 
@@ -153,6 +160,110 @@ function AboutUs() {
                         ))}
                     </MarkerClusterGroup>
                 </MapContainer>
+            </div>
+
+            <div className={cx('latest-news')}>
+                <div className={cx('container')}>
+                    <header>
+                        <div>
+                            <h3>Latest News</h3>
+                            <span>See more</span>
+                        </div>
+                        <p>
+                            Mirrors has used, and intends to continue using, its investor relations website and the
+                            Mirrors News Blog <span>(blog.mirrors.com/news) </span>
+                            to disclose material non-public information and to comply with its disclosure obligations
+                            under Regulation FD. Accordingly, you should monitor our investor relations website and the
+                            Etsy News Blog in addition to following our press releases, SEC filings and public
+                            conference calls and webcasts.
+                        </p>
+                    </header>
+                    <div className={cx('news-list')}>
+                        {/* item1 */}
+                        <div className={cx('news-item')}>
+                            <img src="https://s22.q4cdn.com/941741262/files/design/latest-news-1.jpg" alt="" />
+                            <div>
+                                <p>Mirrors to Participate in Upcoming Investor Conferences</p>
+                                <p className={cx('date')}>February 23, 2023</p>
+                            </div>
+                        </div>
+
+                        {/* item2 */}
+                        <div className={cx('news-item')}>
+                            <img src="https://s22.q4cdn.com/941741262/files/design/latest-news-2-new.jpg" alt="" />
+                            <div>
+                                <p>Mir, Inc. Reports Fourth Quarter and Full Year 2022 Results</p>
+                                <p className={cx('date')}>February 22, 2023</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={cx('commitment')}>
+                <div className={cx('container')}>
+                    <div className={cx('header')}>
+                        <h3>Committing to a world of good</h3>
+                        <p>
+                            What does it mean to shop at Mirrors? It means that with every one-of-a-kind,
+                            can’t-stop-talking-about-it, completely delightful item you buy, a wave of positive change
+                            begins. From the small businesses, to the people behind them, to the planet around us – each
+                            purchase creates an impact we can all be proud of. Learn more about our push for progress
+                            and join us in committing to a world of good.
+                        </p>
+                    </div>
+
+                    <video autoPlay muted loop playsInline className={cx('video')}>
+                        <source
+                            src="https://i.etsystatic.com/site-assets/impact/impact-page/header-hero.mp4"
+                            type="video/mp4"
+                        ></source>
+                    </video>
+                </div>
+
+                <div className={cx('purchase-protection')}>
+                    <img src={images.hand} alt="" />
+                    <div className={cx('content')}>
+                        <p>
+                            <strong>Buy confidently </strong>
+                            with Mirrors's Purchase Protection program for buyers, get a full refund in the rare case
+                            your item doesn't arrive, arrives damaged, or isn't as described.{' '}
+                            <Link to="/purchase-protection">See eligibility</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={cx('additional')}>
+                <div className={cx('container')}>
+                    <div>
+                        <div className={cx('video')}>
+                            <video autoPlay muted loop playsInline>
+                                <source
+                                    src="https://i.etsystatic.com/site-assets/impact/impact-page/content-bike.mp4"
+                                    type="video/mp4"
+                                ></source>
+                            </video>
+                        </div>
+
+                        <div className={cx('content')}>
+                            <h3>We believe makers and creatives everywhere deserve a shot at economic independence</h3>
+                            <p>
+                                The Mirrors Uplift Initiative is a series of programmes that opens doors and builds
+                                opportunities for creative entrepreneurs everywhere. In 2021, Mirrors collected over 6.8
+                                million total donations from buyers like you
+                            </p>
+                            <button>Read about the Mirrors Uplift Initiative</button>
+                            <div>
+                                <span>What's next?</span>
+                                <p>
+                                    Mirrors has also partnered with Nest, a non-profit focused on gender equity and
+                                    economic inclusion for makers and artisans.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
